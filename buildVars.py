cf9709c7c1c0ea6f6ca208d2a53332a9540e9621
ruifontes <rui.fontes@tiflotecnia.com>
2021-07-25 17:54:59 +0100

    Version 3.1.5
    Replace speech.getSynth().name to synthDriverHandler.getSynth().name

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Driver and interface for Nuance Vocalizer expressive version 1.1."""),
 	# version
-	"addon_version": "3.1.3",
+	"addon_version": "3.1.5",
 	# Author(s)
 	"addon_author": "Tiflotecnia, LDA. <tiflotecnia@tiflotecnia.com>",
 	# URL for the add-on documentation support
@@ -33,7 +33,7 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": 2019.3,
+	"addon_minimumNVDAVersion": 2021.1,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion": 2021.1,
 	# Add-on update channel (default is None, denoting stable releases,
